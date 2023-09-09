0f4cc1f6fc92809b8bb753a10cd24e02db0b16f3
gabrielakinola <gabrieltemitopeakinola@gmail.com>
2023-09-09 15:44:34 +0100

    Get product query endpoint

--- src/controllers/productController.ts
+++ src/controllers/productController.ts
@@ -1,7 +1,8 @@
 import { Request, Response, NextFunction } from "express";
 import { asyncErrorHandler } from "./errorController";
-import { Product } from "../models/productModel";
+import { Iproduct, Product } from "../models/productModel";
 import { IUser } from "../models/userModel";
+import { CustomEndpointError } from "../classes/errorClasses";
 
 const addProduct = asyncErrorHandler(
   async (req: Request, res: Response, next: NextFunction) => {
@@ -26,4 +27,35 @@ const addProduct = asyncErrorHandler(
   }
 );
 
-export { addProduct };
+const getProducts = asyncErrorHandler(
+  async (req: Request, res: Response, next: NextFunction) => {
+    const { name } = req.query;
+
+    if (name) {
+      const products: Iproduct[] = await Product.find({
+        name,
+      });
+
+      const productNotStrict: Iproduct[] = await Product.find({
+        name: { $regex: name, $options: "i" },
+      });
+      if (products.length === 0 && productNotStrict.length === 0) {
+        throw new CustomEndpointError("No result matches your query", 404);
+      } else if (products.length > 0) {
+        res.status(200).json({ products });
+      } else if (productNotStrict.length > 0) {
+        res.status(200).json({ products: productNotStrict });
+      }
+    } else {
+      const products: Iproduct[] = await Product.find();
+
+      if (products.length > 0) {
+        res.status(200).json({ products });
+      } else {
+        throw new CustomEndpointError("Not Found", 404);
+      }
+    }
+  }
+);
+
+export { addProduct, getProducts };
--- src/routes/productRoutes.ts
+++ src/routes/productRoutes.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
-import { addProduct } from "../controllers/productController";
+import { addProduct, getProducts } from "../controllers/productController";
 import { ensureIsAuthenicated, isAdmin } from "../middlewares/authentication";
 import { upload } from "../middlewares/multer";
 
 const router = Router();
 
 router.post("/add-product", ensureIsAuthenicated, isAdmin, upload, addProduct);
+
+router.get("/get-product", getProducts);
 
 export default router;
